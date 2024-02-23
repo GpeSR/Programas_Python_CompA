9e985f4fa1e0d685590cd5f5b853e43af0325b82
GpeSR <38192134@uaz.ed.mx>
2024-02-22 19:43:28 -0600

    Actividad 2

--- P08-dividir-cifras.py
+++ P08-dividir-cifras.py
@@ -1,11 +1,11 @@
-# Dividir un numero entero de 3 cifras en sus digitos individuales
+# Dividir un numero entero de 4 cifras en sus digitos individuales
 
 import os; os.system("cls")
 
 print("Dividiendo en unidades, decenas, centenas, y millares un numero entero\n")
 
-#num = int(input("Ingrese un numero entero de 4 cifras: "))
-num = int(input("Ingresa tu año de nacimiento: "))
+num = int(input("Ingrese un numero entero de 4 cifras: "))
+
 
 unidades = num % 10 
 num //= 10 
@@ -15,7 +15,5 @@ centenas = num % 10
 num //= 10
 millares = num 
 
-numSuerte = millares + centenas + decenas + unidades
 
 print(f'Millares: {millares}, Centenas: {centenas}, Decenas: {decenas}, Unidades: {unidades}')
-print(f"\n Tu numero de la suerte es: {numSuerte}")
